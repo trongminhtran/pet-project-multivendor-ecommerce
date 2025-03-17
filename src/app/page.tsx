dbdf819cf91e1638b94153737a1b4c4a4410413b
root <root@MSI.>
2025-03-17 17:23:35 +0000

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-blue-500">Welcome to the course !</h1>
       <h1 className="font-bold text-blue-500 font-barlow">Welcome to the course !</h1>
+      <Button variant="destructive">Click here</Button>
     </div>
   );
 }
